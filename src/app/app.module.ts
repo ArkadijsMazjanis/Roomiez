87f9f905be66647bd32a320ab215e435eda9dba4
Arkādijs <arkaadys@gmail.com>
2021-05-17 11:04:19 +0300

    Ta_7
    Updated componets for dash bord and theyr possition and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
-import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin/admin.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { RegistartionComponent } from './registartion/registartion.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { SharedModule } from './shared/components/shared.module'
+import { SharedModule } from './shared/components/shared.module';
 
 
 
 const appRoutes: Routes = [
-  { path: '', component: LandingpageComponent },
+  { path: '', component: LandingpageComponent },  //direct aproach and routs
   { path: 'login', component: LoginComponent },
-  { path: 'login', component: DashboardComponent },
   { path: 'register', component: RegistartionComponent },
-  { path: 'register', component: LoginComponent },
+  { path: 'sidebar', component: SidebarComponent }, 
+  { path: 'header', component: HeaderComponent }, 
   { path: 'dashboard', component: DashboardComponent,
-    children: [
-      { path: '', component: SidebarComponent},
-        { path: '', component: HeaderComponent}
-        
-        
-    ] },
-
-
-  // { path: 'sidebar', component: SidebarComponent }, //direct aproach
-  // { path: 'header', component: HeaderComponent }, //direct aproach
- 
+    // children: [
+    //   { path: '', component: SidebarComponent },
+    //   { path: '', component: HeaderComponent }     
+    },
 ];
 
 @NgModule({
@@ -44,6 +37,7 @@ const appRoutes: Routes = [
     AdminComponent,
     LandingpageComponent,
     RegistartionComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -56,6 +50,7 @@ const appRoutes: Routes = [
   ],
   exports: [
     RouterModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/shared.module.ts
+++ src/app/shared/components/shared.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common' 
 import { SidebarComponent } from './sidebar/sidebar.component'
-import { RouterModule } from '@angular/router';
+import { RouterModule, } from '@angular/router';
 import { HeaderComponent } from './header/header.component'
+
 
 @NgModule({
     declarations: [
         SidebarComponent,
-        HeaderComponent
+        HeaderComponent,
+        
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
 
     ],
     exports: [
         SidebarComponent,
+        HeaderComponent
     ]
 })
 
